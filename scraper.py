8e10dbf20b879af3d65ef658f0f99ca26db90f02
scott <bogenscott@gmail.com>
2020-11-07 22:20:23 -0800

    scraper fixed, prototyping system redesign

--- scraper.py
+++ scraper.py
@@ -7,8 +7,12 @@ from song import Song
 # TODO: Sometimes (probably on long song names) the Genius API returns wrong results even though the song
 # is in the Genius system. Probalby the next step should be to just search by the song title.
 
+# TODO: Fix the reprs
 
-def get_token():
+# TODO: When song lyrics weren't found, it just kept printing the "no lyrics found" message. This happened with classical music. Address this.
+
+
+def read_token():
     f = open('token.txt', 'r')
     token = f.read()
     return token
@@ -36,13 +40,6 @@ def print_artist_and_title(song):
 
 
 def get_genius_results(song, base_url, headers):
-    token = get_token()
-
-    print("token = " + token)
-
-    base_url = "http://api.genius.com"
-    headers = {'Authorization': 'Bearer ' + token}
-
     search_url = base_url + "/search"
     params = {'q': song.title + ' ' + song.artist}
     response = requests.get(search_url, params=params, headers=headers)
@@ -57,12 +54,29 @@ def verify_song(song, response):
     return None
 
 
+"""
+    Main class:
+        Start of program
+        Verifies connectivity with Spotify API (else errors out)
+        Verifies connectivity with Genius API (else errors out)
+
+        Runs main loop (ESC exits it) on a 5 second sleep interval:
+            Call "connector" Spotify, get the currently playing song
+            If there is such a song and it has not been displayed, send it to GeniusConnector, get the genius API path of the song
+            If there is such an API path, send it to the LyricsParser class to get the lyrics
+            If the LyricsParser class returns the lyrics, then print them
+
+    I will do this when at home.
+"""
+
 # this should probably be its own class
 if __name__ == '__main__':
+    base_url = "http://api.genius.com"
 
-    # former connector logic
+    token = read_token()
+    headers = {'Authorization': 'Bearer ' + token}
+
     scope = "user-read-currently-playing"
-
     song_fetcher = connector.SongFetcher(scope)
     most_recent_song = Song('', '')
 
@@ -73,17 +87,15 @@ if __name__ == '__main__':
             # call genius API
             most_recent_song = current_song
 
-            response = get_genius_results(current_song)
+            response = get_genius_results(current_song, base_url, headers)
             song_info = verify_song(current_song, response)
 
             # is the song is found
             if song_info:
-
-                # print  'artist - title'
                 print_artist_and_title(current_song)
                 song_api_path = song_info['result']['api_path']
-
-                print(get_lyrics(song_api_path))
+                lyrics = get_lyrics(song_api_path, base_url, headers)
+                print(lyrics)
 
             else:
                 print("Song lyrics not found :-(")
--- song.py
+++ song.py
@@ -2,3 +2,9 @@ class Song:
     def __init__(self, title, artist):
         self.title = title
         self.artist = artist
+
+    def __str__():
+        return self.artist + ' - ' + self.title
+
+    def __repr__():
+        return self.artist + self.title
